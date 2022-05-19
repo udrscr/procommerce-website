3b8e522a8c049bbf0a18df562d11eab688c586db
DanicaTo <danicatomic@ymail.com>
2022-05-19 15:07:46 +0200

    wp plugin url edited

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
          // the only required plugin option for WordPress is the GraphQL url.
          url:
            process.env.WPGRAPHQL_URL ||
-           `https://wpgatsbydemo.wpengine.com/graphql`,
+           `https://createq.udrscr.dev/graphql`,
        },
     },
 
